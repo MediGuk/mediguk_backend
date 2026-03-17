583a971f91fedc08624f2eb9bb0dc26ad8758efd
mendibot <mendibot15@gmail.com>
2026-03-17 22:46:07 +0100

    feat(triage): update conntroller route

--- src/main/java/com/mediguk/backend/triage/controller/TriageController.java
+++ src/main/java/com/mediguk/backend/triage/controller/TriageController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/v1/triage")
+@RequestMapping("/api/triage")
 @RequiredArgsConstructor
 public class TriageController {
 
